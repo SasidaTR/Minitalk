ef52ec6ebc8db1b9208af9cddab832ca4358dad8
Tommy Rischmann <trischma@made-f0Cr13s1.clusters.42paris.fr>
2024-06-21 10:45:37 +0200

    Push automatique :)

--- include/minitalk.h
+++ include/minitalk.h
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hrischma <hrischma@student.42.fr>          +#+  +:+       +#+        */
+/*   By: trischma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 16:31:15 by hrischma          #+#    #+#             */
-/*   Updated: 2024/06/06 16:31:16 by hrischma         ###   ########.fr       */
+/*   Updated: 2024/06/21 10:45:15 by trischma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
+
 # include <sys/types.h>
 # include <unistd.h>
 # include <stdlib.h>
